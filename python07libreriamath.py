a5d0a918000f6f67e5c4f9209f16efdf4451c358
jmdsgithub <josemendezds@gmail.com>
2025-02-07 20:53:25 +0100

    Vie 07-03 20.53

---
+++ python07libreriamath.py
@@ -0,0 +1,15 @@
+print("Ejemplo de librerias")
+#Sintaxis con from
+from math import floor, ceil, trunc
+numero1 = 20
+numero2 = 3
+division = numero1 / numero2 
+print("la divisiòn es ", division)
+#declaramos variables para almacenar valores
+varFloor = floor(division)
+varCeil = ceil(division)
+varTunc = trunc(division)
+print("Floor ", varFloor)
+print("Ceil ", varCeil)
+print("Trunc ", varTrunc)
+print("Fin de programa")
