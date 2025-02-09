aa9b08b94d1ff29d17339f5bb7dc95c2cc7e3a5f
jmdsgithub <josemendezds@gmail.com>
2025-02-09 15:25:01 +0100

    2025-02-09.15-24h

---
+++ python02variables.py
@@ -0,0 +1,10 @@
+print("Ejemplo variables")
+# esto es un comentario
+# comentar: command + k + c
+# descomentar: command + k + u
+# Las variables no se declaran con tipado
+# las variables las declaramos en minúscula
+numero = 99
+texto = "Variable tipo texto (string)"
+print(numero)
+print(texto) 
--- python02variables1.py
+++ python02variables1.py
@@ -1,9 +1,9 @@
 print("Ejemplo variables")
-#esto es un comentario
-#comentar: command + k + c
-#descomentar: command + k + 
-#Las variables no se declaran con tipado
-#las variables las declaramos en minúscula
+# esto es un comentario
+# comentar: command + k + c
+# descomentar: command + k + u
+# Las variables no se declaran con tipado
+# las variables las declaramos en minúscula
 numero = 99
 texto = "variable tipo texto (string)"
 print("El número es: ", numero)
